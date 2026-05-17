85255b9f3b1587da28f9f7fd5377f677534eea13
chushiyu111-design <chushiyu111@users.noreply.github.com>
2026-05-17 19:51:26 +0800

    chore(apps): hide experimental launchers

--- constants.tsx
+++ constants.tsx
@@ -119,6 +119,9 @@ export const APP_CONFIGS: AppConfig[] = [
 
 const HIDDEN_LAUNCHER_APPS = new Set<AppID>([
   AppID.Crosstime,
+  AppID.Theater,
+  AppID.Trajectory,
+  AppID.LoveShow,
 ]);
 
 export const INSTALLED_APPS: AppConfig[] = APP_CONFIGS.filter(app => !HIDDEN_LAUNCHER_APPS.has(app.id));
